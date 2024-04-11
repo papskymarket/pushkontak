6bdd744badf3c76dcd88526e8da5ac45ac5edd0d
papskymarket <165714696+papskymarket@users.noreply.github.com>
2024-04-12 00:15:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
 global.owner = ["6285380000995"]
-global.push_kontak_delay = 8000 //8 detik, 1000 = 1 detik
+global.push_kontak_delay = 15000 //15 detik, 1000 = 1 detik
 global.pairingNumber = ""
